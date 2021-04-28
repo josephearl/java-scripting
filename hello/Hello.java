cd320bbdbb171d354b2263d39ea594bbcc10a27e
Joseph Earl <jearl@thoughtworks.com>
2021-04-28 11:31:30 +0100

    Print args

--- hello/Hello.java
+++ hello/Hello.java
@@ -2,6 +2,6 @@ package hello;
 
 class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello Java!");
+    System.out.println("Hello Java! [" + String.join(",", args) + "]");
   }
 }
